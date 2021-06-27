406d036a37452aabf0107b5fc55a78a2749aa747
Monica-2410 <86554420+Monica-2410@users.noreply.github.com>
2021-06-27 16:12:36 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Info from "./Info";
+import Inf from "./Inf";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+      <Info />
+      <Inf />
+    </div>
+  );
+}
+export default App;
---
+++ Inf.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Inf() {
+  return (
+    <div className="note">
+      <h1>Python and Cybersecurity</h1>
+      <p>A basic python and cyber security bootcamp</p>
+    </div>
+  );
+}
+
+export default Inf;
